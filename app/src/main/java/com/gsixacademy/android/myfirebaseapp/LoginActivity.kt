3648047550f03c7fe4d1d7fa7b49c32ea1671519
antoniojovanoski <jovanoskiantonio1998@gmail.com>
2020-08-08 16:50:18 +0200

    Initial commit

---
+++ app/src/main/java/com/gsixacademy/android/myfirebaseapp/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.gsixacademy.android.myfirebaseapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        button_login.setOnClickListener{
+            Toast.makeText(this,"",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
